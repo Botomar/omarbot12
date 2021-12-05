ccfaa6a2b9067bd64c054e92f0c789eef33f1ed3
Botomar <86120193+Botomar@users.noreply.github.com>
2021-12-04 22:41:48 -0500

    Update levelmenu.js

--- src/levelmenu.js
+++ src/levelmenu.js
@@ -3,7 +3,7 @@ const levelmenu = (prefix, pushname) => {
 
 *Comandos De Level 🆕 beta*
  
-══════════════
+╰┅✺⃘⃙⃭͜͡𐝠⃙࿆⁃⃭⊱⃙༘ࣼ༅⃭═⃙⃭❅⃙⃭•⁃⃟ࣼ🎋⃟ࣼ⁃•⃙⃬❅⃙⃬═༘⃬ࣼ༅⃙⃬⃗⁃⃬𐝠⃙࿆⃬✺⃘⃖͜͡┅⃬╯͑͗͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑
 
 PARA ACTIVARLO EL BOT NECESITA DE ADMINISTRACIÓN
 
@@ -16,17 +16,16 @@ PARA ACTIVARLO EL BOT NECESITA DE ADMINISTRACIÓN
 
 *leveling 0
 
-══════════════
+╰┅✺⃘⃙⃭͜͡𐝠⃙࿆⁃⃭⊱⃙༘ࣼ༅⃭═⃙⃭❅⃙⃭•⁃⃟ࣼ🎋⃟ࣼ⁃•⃙⃬❅⃙⃬═༘⃬ࣼ༅⃙⃬⃗⁃⃬𐝠⃙࿆⃬✺⃘⃖͜͡┅⃬╯͑͗͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑
 
 PARA VER TU XP EN TIEMPO REAL
 
 *level
-
-══════════════
+╰┅✺⃘⃙⃭͜͡𐝠⃙࿆⁃⃭⊱⃙༘ࣼ༅⃭═⃙⃭❅⃙⃭•⁃⃟ࣼ🎋⃟ࣼ⁃•⃙⃬❅⃙⃬═༘⃬ࣼ༅⃙⃬⃗⁃⃬𐝠⃙࿆⃬✺⃘⃖͜͡┅⃬╯͑͗͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑
     
 *Recuerda que si actualizas tu bot o borras el numero del bot o otra cosa todos los levels de los particiantes quedaran en 0*
     
-_*by shanduy*_
+_*🇵🇪Loco Humilde y Real🇵🇪*_
 `
 
 }
