b8e7c752d1ae8420c497fe220aa49948281808e4
Botomar <86120193+Botomar@users.noreply.github.com>
2021-12-04 22:35:33 -0500

    Update antimenu.js

--- src/antimenu.js
+++ src/antimenu.js
@@ -1,76 +1,77 @@
 const antimenu = (prefix, pushname) => {
     return `
 
-*Comandos Para Activar Los Antilinks 📢*
+*🤹Comandos Para Activar Los Antilinks 🤹*
 
-══════════════
+╰┅✺⃘⃙⃭͜͡𐝠⃙࿆⁃⃭⊱⃙༘ࣼ༅⃭═⃙⃭❅⃙⃭•⁃⃟ࣼ🎋⃟ࣼ⁃•⃙⃬❅⃙⃬═༘⃬ࣼ༅⃙⃬⃗⁃⃬𐝠⃙࿆⃬✺⃘⃖͜͡┅⃬╯͑͗͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑
 
 _*Antilink De Grupos De Whatsapp*_
 
-*PARA ACTIVAR EL ANTILINK*
+*🤹PARA ACTIVAR EL ANTILINK🤹*
 ${prefix}antilink 1
 
-*PARA DESACTIVAR EL ANTILINK*
+*🤹PARA DESACTIVAR EL ANTILINK🤹*
 ${prefix}antilink 0
 
-══════════════
+╰┅✺⃘⃙⃭͜͡𐝠⃙࿆⁃⃭⊱⃙༘ࣼ༅⃭═⃙⃭❅⃙⃭•⁃⃟ࣼ🎋⃟ࣼ⁃•⃙⃬❅⃙⃬═༘⃬ࣼ༅⃙⃬⃗⁃⃬𐝠⃙࿆⃬✺⃘⃖͜͡┅⃬╯͑͗͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑
 
-_*Antilink De Instagram*_
+_*🤹Antilink De Instagram🤹*_
 
-*PARA ACTIVAR EL ANTILINK*
+*🤹PARA ACTIVAR EL ANTILINK🤹*
 ${prefix}antinsta 1
 
-*PARA DESACTIVAR EL ANTILINK*
+*🤹PARA DESACTIVAR EL ANTILINK🤹*
 ${prefix}antinsta 0
 
-══════════════
+╰┅✺⃘⃙⃭͜͡𐝠⃙࿆⁃⃭⊱⃙༘ࣼ༅⃭═⃙⃭❅⃙⃭•⁃⃟ࣼ🎋⃟ࣼ⁃•⃙⃬❅⃙⃬═༘⃬ࣼ༅⃙⃬⃗⁃⃬𐝠⃙࿆⃬✺⃘⃖͜͡┅⃬╯͑͗͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑
 
-_*Antilink De Facebook*_
+_*🤹Antilink De Facebook🤹*_
 
-*PARA ACTIVAR EL ANTILINK*
+*🤹PARA ACTIVAR EL ANTILINK🤹*
 ${prefix}antiface 1
 
-*PARA DESACTIVAR EL ANTILINK*
+*🤹PARA DESACTIVAR EL ANTILINK🤹*
 ${prefix}antiface 0
 
-══════════════
+╰┅✺⃘⃙⃭͜͡𐝠⃙࿆⁃⃭⊱⃙༘ࣼ༅⃭═⃙⃭❅⃙⃭•⁃⃟ࣼ🎋⃟ࣼ⁃•⃙⃬❅⃙⃬═༘⃬ࣼ༅⃙⃬⃗⁃⃬𐝠⃙࿆⃬✺⃘⃖͜͡┅⃬╯͑͗͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑
 
-_*Antilink De Tik Tok*_
+_*🤹Antilink De Tik Tok🤹*_
 
-*PARA ACTIVAR EL ANTILINK*
+*🤹PARA ACTIVAR EL ANTILINK🤹*
 ${prefix}antitik 1
 
-*PARA DESACTIVAR EL ANTILINK*
+*🤹PARA DESACTIVAR EL ANTILINK🤹*
 ${prefix}antitik 0
 
-══════════════
+╰┅✺⃘⃙⃭͜͡𐝠⃙࿆⁃⃭⊱⃙༘ࣼ༅⃭═⃙⃭❅⃙⃭•⁃⃟ࣼ🎋⃟ࣼ⁃•⃙⃬❅⃙⃬═༘⃬ࣼ༅⃙⃬⃗⁃⃬𐝠⃙࿆⃬✺⃘⃖͜͡┅⃬╯͑͗͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑
 
 
-_*Antilink De Kwai*_
+_*🤹Antilink De Kwai🤹*_
 
-*PARA ACTIVAR EL ANTILINK*
+*🤹PARA ACTIVAR EL ANTILINK🤹*
 ${prefix}antikwai 1
 
-*PARA DESACTIVAR EL ANTILINK*
+*🤹PARA DESACTIVAR EL ANTILINK🤹*
 ${prefix}antikwai 0
  
-══════════════
+╰┅✺⃘⃙⃭͜͡𐝠⃙࿆⁃⃭⊱⃙༘ࣼ༅⃭═⃙⃭❅⃙⃭•⁃⃟ࣼ🎋⃟ࣼ⁃•⃙⃬❅⃙⃬═༘⃬ࣼ༅⃙⃬⃗⁃⃬𐝠⃙࿆⃬✺⃘⃖͜͡┅⃬╯͑͗͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑
 
 
-_*Antilink De Discord*_
+_*🤹Antilink De Discord🤹*_
 
-*PARA ACTIVAR EL ANTILINK*
+*🤹PARA ACTIVAR EL ANTILINK🤹*
 ${prefix}antidiscord 1
 
-*PARA DESACTIVAR EL ANTILINK*
+*🤹PARA DESACTIVAR EL ANTILINK🤹*
 ${prefix}antidiscord 0
  
-══════════════
+╰┅✺⃘⃙⃭͜͡𐝠⃙࿆⁃⃭⊱⃙༘ࣼ༅⃭═⃙⃭❅⃙⃭•⁃⃟ࣼ🎋⃟ࣼ⁃•⃙⃬❅⃙⃬═༘⃬ࣼ༅⃙⃬⃗⁃⃬𐝠⃙࿆⃬✺⃘⃖͜͡┅⃬╯͑͗͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑͗͑
 
 _Para activar estos comandos el bot necesita tener admin en el grupo y esta funcion solo la activan los admins del grupo_
 
+*🤹OMAR-BOT🤹*
 
-_*by shanduy*_
+_*🇵🇪Loco Humilde y Real 🇵🇪*_
 `
 
 }
